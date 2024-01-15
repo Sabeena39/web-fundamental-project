799f6096a311ec947aca03b6a9c8fd7cda7ce2fe
sabeena <144470044+Sabeena39@users.noreply.github.com>
2024-01-15 21:22:26 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    if (localStorage.getItem('user')) {
+        loadPage('order');
+    } else {
+        loadPage('login');
+    }
+};
+
+function loadPage(page) {
+    var a = new XMLHttpRequest();
+    a.open('GET', page + '.html', true);
+    a.onreadystatechange = function() {
+        if (this.readyState !== 4) return;
+        if (this.status !== 200) return;
+        document.querySelector('.logindiv').innerHTML = this.responseText;
+
+        if (page === 'login') {
+            document.getElementById('loginForm').addEventListener('submit', function(e) {
+                e.preventDefault();
+                var username = document.getElementById('username').value;
+                var password = document.getElementById('password').value;
+
+                if (username === password) {
+                    localStorage.setItem('user', username);
+                    localStorage.setItem('password', password);
+                    alert('Login Successful');
+                    window.location.href = 'order.html';
+                } else {
+                    alert('Please enter valid credentials!');
+                }
+            });
+        }
+    };
+    a.send();
+}
